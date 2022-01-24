f0ba58aa66b9a70450755b004e8e6d6c8e3a4aa2
Himanshu Sharma <78699352+himanshusharma9034@users.noreply.github.com>
2022-01-25 02:04:06 +0530

    Add files via upload

---
+++ Programming_Assignment1.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#1. Write a Python program to print "Hello Python"?
+print("Hello Python")
+
+
+# In[4]:
+
+
+# 2. Write a Python program to do arithmetical operations addition and division.?
+a = int(input())
+b = int(input())
+print(a+b, a/b)
+
+
+# In[5]:
+
+
+# 3. Write a Python program to find the area of a triangle?
+height = int(input())
+base = int(input())
+print("Area of a triangle = ", height*base/2)
+
+
+# In[12]:
+
+
+# 4. Write a Python program to swap two variables?
+a = input()
+b = input()
+c = a
+a = b
+b = c
+print(a,b)
+
+
+# In[39]:
+
+
+# 5. Write a Python program to generate a random number?
+import random
+print("Generate a random number : ", random.random())
+
+
+# In[ ]:
+
+
+
+
